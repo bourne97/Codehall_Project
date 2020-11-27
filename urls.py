471902acc5b614e681186c9fb99c285a07c19bde
bourne97 <41855050+bourne97@users.noreply.github.com>
2020-11-27 10:04:07 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,22 +1,19 @@
-"""file_manager URL Configuration
+from django.urls import path
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import include, path
+from . import views
+from .views import LoginView, RegisterView, UploadFileView, CreateFolderView
 
 urlpatterns = [
-    path('', include('app.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('login', LoginView.as_view(), name='login'),
+    path('create_account', RegisterView.as_view(), name='create_account'),
+    path('logout', views.logout_view, name='logout'),
+    path('upload_file', UploadFileView.as_view(), name='upload_file'),
+    path('create_folder', CreateFolderView.as_view(), name='create_folder'),
+    path('files/<int:id>/<int:folder>', views.files, name='files'),
+    path('view_file/<int:id>', views.view_file, name='view_file'),
+    path('download_file/<int:id>', views.download_file, name='download_file'),
+    path('delete_file/<int:id>', views.delete_file, name='delete_file'),
+    path('search_user_files/<int:user_id>', views.search_user_files, name='search_user_files'),
+    path('search_all_files', views.search_all_files, name='search_all_files')
 ]
